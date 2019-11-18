fd8fa45977758fc5648236b3bc362a71b6264355
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-11-18 18:39:14 -0500

    Rendering the basics of the contact list menu.

--- src/components/contact-list/ContactList.jsx
+++ src/components/contact-list/ContactList.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 
+import "./contact-list.scss";
+
+//Have to add this component below a scrollbar region like the sidebar and messageList
+
 const ContactList = () => {
     return (
-        <div className="new-div">
-            </div>
+        <div className="contact-list">
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+                
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+
+                <div className="user-card">
+                <img alt="Image of John Miller"></img>
+                <div className="user-text-container">
+                    <h2>John Miller</h2>
+                    <p>Hi Erin, we'll be meeting on Friday to discuss the proposal...</p>
+                </div>
+                </div>
+
+        </div>
     );
 }
 
