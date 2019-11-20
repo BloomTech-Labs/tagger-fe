cb5988a67c4149499fa50749b7ffc64e62a81e06
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-11-20 10:33:24 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppContainer from "./AppContainer";
 
 import "./App.css";
-
+///App
 const App = () => {
-
   return (
     <Router>
       <AppContainer />
     </Router>
   );
-}
+};
 
 export default App;
