738a2a6a5c0a418187b3bb1a8b7b532da5dba8a4
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-11-20 10:32:01 -0800

    testing

--- config.jsx
+++ config.jsx
@@ -1,4 +1,7 @@
 export const config = {
-    CLIENT_ID: '498525641423-gv4h1poto9mdbdlj7qibo9sf0t4f2231.apps.googleusercontent.com', // Must be taken from Google API Console (https://console.developers.google.com/)
-    API_KEY: 'AIzaSyBZz8ZGWxwOo4BTA0-h5dzqiGs3n9tgoAU' // https://developers.google.com/maps/documentation/javascript/get-api-key
-}
+  CLIENT_ID:
+    "498525641423-gv4h1poto9mdbdlj7qibo9sf0t4f2231.apps.googleusercontent.com", // Must be taken from Google API Console (https://console.developers.google.com/)
+  API_KEY: "AIzaSyBZz8ZGWxwOo4BTA0-h5dzqiGs3n9tgoAU" // https://developers.google.com/maps/documentation/javascript/get-api-key
+};
+
+//Config
