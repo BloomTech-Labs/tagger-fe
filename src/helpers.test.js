77db880b7003139f4d39b055eb1083b5607e7e9b
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-12-06 14:42:34 -0500

    Imported more dependencies so we can test JSX files.

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,10 +1,17 @@
+// import React from 'react';
+// import renderer from 'react-test-renderer';
+const React = require('react');
+const Render = require('react-test-renderer');
+import {toggle} from './components/main/Main';
+
 const help = require('./helpers');
+
 
 describe('sum function', () => {
   // This is a describe block. You can nest them,
   // to group your tests
 
-  
+
      // This is a test block. Note the test runner
      // is reporting a success. Not so soon!
      // This test is not done.
@@ -14,3 +21,12 @@ describe('sum function', () => {
     expect(actual).toBe(expected); // .toBe() looks for strict equality!
   });
 }); 
+
+
+//write some fake business logic into this file if I need it. test the expected value for that made-up example. import functions from my other files if needed. 
+
+describe('toggles between dashboards', () => {
+    it('toggle is false by default', () => {
+        expect(toggle).toBe(false);
+    });
+});
