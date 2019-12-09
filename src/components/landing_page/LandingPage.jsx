441c0b3e5069dfb4a803aaad7c8ed8bfd64bacb9
mchrupcala <michaelchrupcala@Michaels-MacBook-Pro.local>
2019-12-09 16:52:02 -0500

    Added marketing copy to the site.

--- src/components/landing_page/LandingPage.jsx
+++ src/components/landing_page/LandingPage.jsx
@@ -35,8 +35,8 @@ const LandingPage = () => {
                 <div className="landing-section landing-section1">
                     <div className="landing-feature feature-1">
                         <div>
-                            <h3>Lorem Ipsum.</h3>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, delectus dignissimos. Sunt eligendi labore quod, temporibus placeat obcaecati officiis, est ipsam harum animi voluptatem vel qui iste! Tempore, tempora! Magni.</p>
+                            <h3>Stay Connected.</h3>
+                        <p>Whoever you're talking to -- clients, friends, or family -- stay in touch and see your connections right in your dashboard. You'll see stats that show you how frequently you email eachother, the last date that you contacted them, and more, right next to your messages.</p>
                         </div>
                         <img className="product-img" src={img2}></img>
                     </div>
@@ -45,16 +45,16 @@ const LandingPage = () => {
                     <div className="landing-feature feature-2">
                     <img className="product-img" src={img3}></img>
                     <div>
-                            <h3>Lorem Ipsum.</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, delectus dignissimos. Sunt eligendi labore quod, temporibus placeat obcaecati officiis, est ipsam harum animi voluptatem vel qui iste! Tempore, tempora! Magni.</p>
+                            <h3>Gmail, but better.</h3>
+                    <p>Our team's spent a lot of time with Gmail -- and even MORE time getting frustrated with it. Tagger is powered by your Gmail account and features a new & improved design, custom-made for your modern life.</p>
                     </div>
                     </div>
                     <div className="bottom-border"></div>
 
                     <div className="landing-feature feature-3">
                     <div>
-                            <h3>Lorem Ipsum.</h3>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque, delectus dignissimos. Sunt eligendi labore quod, temporibus placeat obcaecati officiis, est ipsam harum animi voluptatem vel qui iste! Tempore, tempora! Magni.</p>
+                            <h3>Work smarter, not harder.</h3>
+                    <p>Have you ever spent too long searching for an old email thread? Us too! Tagger was built to auto-tag and sort your messages using advanced Machine Learning tools, so you'll never lose track of the conversations that matter to you.</p>
                     </div>
                     <img className="product-img" src={img4}></img>
                     </div>
