b66ae6100e54bce7b83913f43eda042d7b9e2f61
teddyhn <tngo97@gmail.com>
2019-12-11 13:54:39 -0800

    Emails within thread are rendering separately

--- src/components/content/message-list/thread-content/ThreadMessage.jsx
+++ src/components/content/message-list/thread-content/ThreadMessage.jsx
@@ -1,17 +1,40 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import { getMessage } from "../../../../api/";
 
 const ThreadMessage = (props) => {
-    const iframeRef = React.createRef();
+    const [emailBody, setEmailBody] = useState();
+
+    const iframeReff = React.createRef();
+    
+    console.log(iframeReff);
 
     useEffect(() => {
         const messageId = props.id;
-        getMessage(messageId).then(res => console.log(res));
+        getMessage(messageId).then(res => setEmailBody(res.body));
     }, []);
 
+    useEffect(() => {
+        if (iframeReff.current) {
+            const { body } = iframeReff.current.contentWindow.document;
+            body.style.margin = "0px";
+            body.style.fontFamily = "Arial, Helvetica, sans-serif";
+            body.style.fontSize = "13px";
+            body.innerHTML = emailBody;
+        }
+    }, [emailBody]);
+
     return (
-        <div>{props.id}</div>
+        <React.Fragment>
+            <iframe
+                ref={iframeReff}
+                title="Thread message"
+                id={props.id}
+                style={{
+                    display: "block"
+                }}
+            />
+        </React.Fragment>
     )
 }
 
