9e7501203acad0121a1c891c00d29d55a468990a
Marcus Jones <indelibleink84@gmail.com>
2019-12-13 12:15:41 -0600

    two more test passing

--- src/reducers/gapi.reducers.spec.js
+++ src/reducers/gapi.reducers.spec.js
@@ -1,4 +1,5 @@
 
+
 // Run npm test -- --coverage (note extra -- in the middle) to include a coverage report like this:
 // ADD TO PACKAGE.JSON
 // RUN COVERAGE TEST
@@ -7,11 +8,16 @@
 //     "presets": ["@babel/preset-env"]
 //   }
 
+
+
+import React from 'react'
+import reducer from './gapi.reducers'
+
+
+
 describe('signedOutReducer reducer', () => {
     it('should return initial state', () => {
-        expect(reducer(undefined, {})).toEqual([
-            {}
-        ])
+        expect(reducer(undefined, {})).toEqual({})
     })
     it('should handle SIGNED_OUT', () => {
         expect()
@@ -19,12 +25,21 @@ describe('signedOutReducer reducer', () => {
 })
 
 describe('actions', () => {
-    it('should create an action to add a todo', () => {
+    it('should create an action to SIGN OUT', () => {
       const state = 'signed out'
       const expectedAction = {
         state: state.SIGNED_OUT,
         state
       }
       expect(actions.signInStatusResult(text)).toEqual(expectedAction)
+    })
+
+    it('should change state to AUTH_IN_PROGRESS', () => {
+        const state = 'auth_in_progress'
+        const expectedAction = {
+            state: state.AUTH_IN_PROGRESS,
+            ...state
+        }
+        expect(actions.signInStatusResult().toEqual(expectedAction))
     })
   })
