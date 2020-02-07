c08f0cb42e621852e1711f974ab54ef71c9f3769
Nathaniel Mosco <natemosco@gmail.com>
2020-02-07 18:41:29 -0500

    added font awesome icons
    Co-authored-by: Andrew Wilson <andrew.wilson90@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import "../node_modules/font-awesome/css/font-awesome.css";
 
 import { store } from "./reducers";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+    document.getElementById("root")
 );
