c20650c3920cc64467919218b7e15e3be6c76027
Nathaniel Mosco <natemosco@gmail.com>
2020-02-20 21:09:19 -0500

    add ellipses back to the subject of Snippet.js in subject

--- src/components/inbox/Snippet.js
+++ src/components/inbox/Snippet.js
@@ -56,11 +56,17 @@ const S = {
     `,
     Subject: styled.div`
         font-weight: 700;
+        width: 100%;
+        text-align: left;
+        overflow: hidden;
+        white-space: nowrap;
+        word-break: break-word;
+        text-align: left;
+        text-overflow: ellipsis;
     `,
     Message: styled.div`
         width: 100%;
         text-align: left;
-        overflow: hidden;
         overflow: hidden;
         white-space: nowrap;
         word-break: break-word;
@@ -69,88 +75,80 @@ const S = {
     `
 };
 
-const Snippet = props => {
- 
-  const setThreadContact = () => {
-    
-    const emailObj = props.email;
-    console.log(emailObj, "WHAT IS THIS ONE THEN?")
-    props.changeThreadContact(emailObj);
-  };
+const Snippet = (props) => {
+    const setThreadContact = () => {
+        const emailObj = props.email;
+        console.log(emailObj, "WHAT IS THIS ONE THEN?");
+        props.changeThreadContact(emailObj);
+    };
 
-  function showDate() {
+    function showDate() {
+        let formatDate;
+        if (typeof props.email.date === "string") {
+            if (props.email.date.includes("T") || props.email.date.includes("-")) {
+                formatDate = new Date(props.email.date);
+            } else {
+                formatDate = new Date(Number(props.email.date));
+            }
+        } else {
+            formatDate = new Date(props.email.date);
+        }
 
-    let formatDate;
-    if(typeof props.email.date === "string") {
-      if (props.email.date.includes("T") || props.email.date.includes("-")){
-        formatDate = new Date(props.email.date)
-      } else {
-        formatDate = new Date(Number(props.email.date))
-      }
-    } else {
-      formatDate = new Date(props.email.date)
+        let emailDateYear = moment(formatDate).format("YYYY");
+        let currentYear = moment().format("YYYY");
+        if (emailDateYear === currentYear) {
+            return moment(formatDate).format("MMM Do");
+        } else {
+            return moment(formatDate).format("MMM Do YYYY");
+        }
     }
 
+    const setAnalyticsContact = (email) => {
+        console.log("EMAIL", email);
 
-    let emailDateYear = moment(formatDate).format("YYYY");
-    let currentYear = moment().format("YYYY");
-    if (emailDateYear === currentYear) {
-        return moment(formatDate).format("MMM Do");
-    } else {
-        return moment(formatDate).format("MMM Do YYYY");
-    }
-  }
+        const filter = props.contacts.filter(
+            (c) => c.emailAddresses[0].value.toLowerCase() === email.from.toLowerCase()
+        );
+        console.log("FILTER", filter);
+        if (filter.length > 0) {
+            const contact = {
+                emailAddress: filter[0].emailAddresses,
+                name: filter[0].names[0].displayName,
+                coverPhoto: filter[0].photos[0].url
+            };
+            // console.log("IF", filter);
+            props.changeAnalyticsContact(contact);
+            props.changeIsDisplayingAnalytics(true);
+        } else {
+            const contact = {
+                emailAddress: [{ value: email.from }],
+                name: email.name
+            };
+            // Sets contact to be displayed in analytics sidebar
+            props.changeAnalyticsContact(contact);
+            props.changeIsDisplayingAnalytics(true);
+        }
+        // console.log("filter", filter)
+    };
 
-  const setAnalyticsContact = email => {
-    
-    console.log("EMAIL", email)
-    
-    const filter = props.contacts.filter(c => c.emailAddresses[0].value.toLowerCase() === email.from.toLowerCase())
-    console.log("FILTER", filter)
-    if (filter.length > 0) {
-      const contact = {
-        emailAddress: filter[0].emailAddresses,
-        name: filter[0].names[0].displayName,
-        coverPhoto: filter[0].photos[0].url
-      };
-      // console.log("IF", filter);
-      props.changeAnalyticsContact(contact);
-      props.changeIsDisplayingAnalytics(true);
-    } else {
-      const contact = {
-        emailAddress: [{value: email.from}],
-        name: email.name
-      };
-      // Sets contact to be displayed in analytics sidebar
-      props.changeAnalyticsContact(contact);
-      props.changeIsDisplayingAnalytics(true);
-    }
-    // console.log("filter", filter)
-  };
-
-  return (
-    <S.Container
-      heightInPx={
-        props.isDisplayingThread ? (props.isDisplayingAnalytics ? 100 : 80) : 75
-      }
-      onClick={() => setThreadContact()}
-    >
-     
-      <S.SnipHeader>
-        <S.Avatar onClick={() => setAnalyticsContact(props.email)} />
-        <div>
-          <h3 onClick={() => setAnalyticsContact(props.email)}>
-            {props.email.name 
-            ? props.email.name 
-            : props.email.from}
-            </h3>
-            <h3>{showDate(props.email.date)}</h3>
-        </div>
-      </S.SnipHeader>
-      <S.Subject>{props.email.subject}</S.Subject>
-      <S.Message>{props.email.email_body_text}</S.Message>
-    </S.Container>
-  );
+    return (
+        <S.Container
+            heightInPx={props.isDisplayingThread ? (props.isDisplayingAnalytics ? 100 : 80) : 75}
+            onClick={() => setThreadContact()}
+        >
+            <S.SnipHeader>
+                <S.Avatar onClick={() => setAnalyticsContact(props.email)} />
+                <div>
+                    <h3 onClick={() => setAnalyticsContact(props.email)}>
+                        {props.email.name ? props.email.name : props.email.from}
+                    </h3>
+                    <h3>{showDate(props.email.date)}</h3>
+                </div>
+            </S.SnipHeader>
+            <S.Subject>{props.email.subject}</S.Subject>
+            <S.Message>{props.email.email_body_text}</S.Message>
+        </S.Container>
+    );
 };
 
 const mapStateToProps = ({ imap, user, inbox, contacts }) => ({
