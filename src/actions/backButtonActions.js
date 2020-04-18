d99bdaa07c7fbb1ad8429da487b812788aa0cec6
Milo Rastgoo <msrastgoo@gmail.com>
2020-04-17 20:42:44 -0400

    extra reducers and actions removed

--- src/actions/backButtonActions.js
+++
@@ -1,7 +0,0 @@
-export const SET_BACK_BUTTON = "SET_BACK_BUTTON";
-
-export const setBackButton = () => dispatch => {
-    dispatch({
-        type: SET_BACK_BUTTON
-    });
-};
--- src/utils/Cron.js
+++
@@ -1,5 +0,0 @@
-var CronJob = require('cron').CronJob;
-var job = new CronJob('* * * * *', function() {
-  console.log('You will see this message every second');
-}, null, true, 'America/Los_Angeles');
-job.start();
